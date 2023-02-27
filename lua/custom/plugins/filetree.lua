12c9acbfa6c7e50d918bbf05d3dcd1f2e15d4e15
Jorge Morales <jorge.iv.morales@gmail.com>
2023-02-26 22:59:33 -0500

    added autopair and filetree custom plugins

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+		require('neo-tree').setup {}
+	end
+}
