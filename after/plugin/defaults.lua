4d40c8796b1054d73a072fe830568e62b00b826f
Jorge Morales <jorge.iv.morales@gmail.com>
2023-03-04 19:56:24 -0500

    Added commands for navigating through windows/buffers

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -18,6 +18,10 @@ local keymap = vim.keymap
 
 keymap.set("n", "<C-a>", "gg<S-v>G"); -- Select all
 
+-- Increment/Decrement numbers
+keymap.set('n', '+', '<C-a>');
+keymap.set('n', '-', '<C-x>');
+
 -- Split window
 keymap.set("n", "ss", ":split<Return><C-w>w", { silent = true })
 keymap.set("n", "sv", ":vsplit<Return><C-w>w", { silent = true })
@@ -26,3 +30,18 @@ keymap.set("n", "<leader>qq", vim.cmd.Ex)
 -- Move lines
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Delete word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- New tabs
+keymap.set('n', '<leader>nt', ':tabedit<Return>', { silent = true, desc = "[N]ew [T]ab" })
+
+-- Move through windows/buffers
+keymap.set('', 'sh', '<C-w>h', { desc = "Move left" })
+keymap.set('', 'sk', '<C-w>k', { desc = "Move down" })
+keymap.set('', 'sj', '<C-w>j', { desc = "Move up" })
+keymap.set('', 'sl', '<C-w>l', { desc = "Move right" })
+
+-- Close window
+keymap.set('n', '<leader>cw', '<C-w>q', { desc = "[C]lose [Window]" })
