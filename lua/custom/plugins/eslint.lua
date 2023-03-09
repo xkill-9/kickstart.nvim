39a17dec7cfbbc669cc19a8f1882285d7274ccca
Jorge Morales <jorge.iv.morales@gmail.com>
2023-03-09 00:50:17 -0500

    Add eslint config

---
+++ lua/custom/plugins/eslint.lua
@@ -0,0 +1,14 @@
+return {
+	'MunifTanjim/eslint.nvim',
+	version = '*',
+	dependencies = {
+		'neovim/nvim-lspconfig',
+		'jose-elias-alvarez/null-ls.nvim',
+	},
+	config = function()
+		local eslint = require('eslint')
+		eslint.setup({
+			bin = 'eslint_d'
+		})
+	end
+}
