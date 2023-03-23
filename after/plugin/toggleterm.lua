2cdd6b3dfa4a256c9a2d5acc4d8dbc82c996ea95
Jorge Morales <jorge.iv.morales@gmail.com>
2023-03-22 22:14:06 -0500

    Added lazygit support through toggleterm

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,9 @@
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>",
+{ noremap = true, silent = true, desc = "[L]azy [G]it" })
