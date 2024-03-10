2863287350aa4594e9994f8feedf12e8224827b0
Jorge Morales <jorge.iv.morales@gmail.com>
2024-03-10 01:53:40 +0100

    add vim-rzip

--- init.lua
+++ init.lua
@@ -224,6 +224,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'lbrayner/vim-rzip', -- Needed for Yarn 2 Neovim setup https://yarnpkg.com/getting-started/editor-sdks#neovim-native-lsp
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
