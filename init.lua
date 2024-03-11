039e49d70cf3f36b8b2439dfdd5284957e4b6361
Jorge Morales <jorge.iv.morales@gmail.com>
2024-03-11 01:04:12 +0100

    add spell options to vim

--- init.lua
+++ init.lua
@@ -98,6 +98,10 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Spell checker
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
