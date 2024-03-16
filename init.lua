f58e99c25ef6ea090565c1694f70b97a4e691d24
Jorge Morales <jorge.iv.morales@gmail.com>
2024-03-16 18:22:26 +0100

    replace file_browser with find_files in netrw hijacking

--- init.lua
+++ init.lua
@@ -363,11 +363,41 @@ require('lazy').setup({
             require('telescope.themes').get_dropdown(),
           },
           ['file_browser'] = {
-            hijack_netrw = true,
             depth = false,
           },
         },
       }
+
+      -- Hijack netrw with telescope.find_files
+      local find_files_hijack_netrw = vim.api.nvim_create_augroup('find_files_hijack_netrw', { clear = true })
+      -- clear FileExplorer appropriately to prevent netrw from launching on folders
+      -- netrw may or may not be loaded before telescope-find-files
+      -- conceptual credits to nvim-tree and telescope-file-browser
+      vim.api.nvim_create_autocmd('VimEnter', {
+        pattern = '*',
+        once = true,
+        callback = function()
+          pcall(vim.api.nvim_clear_autocmds, { group = 'FileExplorer' })
+        end,
+      })
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = find_files_hijack_netrw,
+        pattern = '*',
+        callback = function()
+          vim.schedule(function()
+            -- Early return if netrw or not a directory
+            if vim.bo[0].filetype == 'netrw' or vim.fn.isdirectory(vim.fn.expand '%:p') == 0 then
+              return
+            end
+
+            vim.api.nvim_buf_set_option(0, 'bufhidden', 'wipe')
+
+            require('telescope.builtin').find_files {
+              cwd = vim.fn.expand '%:p:h',
+            }
+          end)
+        end,
+      })
 
       -- Enable telescope extensions, if they are installed
       pcall(require('telescope').load_extension, 'fzf')
