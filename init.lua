1830c3d3ac16825ee8a4926e981f28ac98d0e04a
Jorge Morales <jorge.iv.morales@gmail.com>
2024-03-25 21:17:53 +0100

    Move nvim-autopairs setup to a dedicated file

--- init.lua
+++ init.lua
@@ -951,15 +951,6 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-  -- Autopairs
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
-  },
-
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
